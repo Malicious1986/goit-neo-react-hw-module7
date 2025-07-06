f3be2483b34a3135a595e5f1596ad31c1ee0ca30
Yurii Osadchyi <v.yurii.osadychi@reddit.com>
2025-07-06 22:57:32 +0100

    loading

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,26 @@
 import { useSelector } from "react-redux";
 
 import styles from "./ContactList.module.css";
-import { selectFilteredContacts } from "../../redux/contactsSlice";
+import {
+  selectFilteredContacts,
+  selectLoading,
+} from "../../redux/contactsSlice";
 import Contact from "../Contact/Contact";
 
 export default function ContactList() {
   const contacts = useSelector(selectFilteredContacts);
-  
+  const isLoading = useSelector(selectLoading);
+
   return (
-    <ul className={styles.container}>
-      {contacts.map((contact) => (
-        <li key={contact.id}>
-          <Contact {...contact} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={styles.container}>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            <Contact {...contact} />
+          </li>
+        ))}
+      </ul>
+      {isLoading && <div>Loading...</div>}
+    </>
   );
 }
